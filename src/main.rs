71b057413a29cd558c0857a16f1a3a78c534ab1f
Samuel Wilson <samWson@users.noreply.github.com>
2021-02-08 21:59:58 +1300

    Implement pipes
    Code is complete to the original tutorial.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::io::{stdin, stdout, Write};
 use std::path::Path;
-use std::process::Command;
+use std::process::{Child, Command, Stdio};
 
 fn main() {
     loop {
@@ -11,29 +11,57 @@ fn main() {
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
 
-        let mut parts = input.trim().split_whitespace();
-        let command = parts.next().unwrap();
-        let args = parts;
+        let mut commands = input.trim().split(" | ").peekable();
+        let mut previous_command = None;
 
-        match command {
-            "cd" => {
-                let new_dir = args.peekable().peek().map_or("/", |x| *x);
-                let root = Path::new(new_dir);
-                if let Err(e) = env::set_current_dir(&root) {
-                    eprintln!("{}", e);
+        while let Some(command) = commands.next() {
+            let mut parts = command.trim().split_whitespace();
+            let command_name = parts.next().unwrap();
+            let args = parts;
+
+            match command_name {
+                "cd" => {
+                    let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                    let root = Path::new(new_dir);
+                    if let Err(e) = env::set_current_dir(&root) {
+                        eprintln!("{}", e);
+                    }
+
+                    previous_command = None;
+                }
+                "exit" => return,
+                command => {
+                    let stdin = previous_command.map_or(Stdio::inherit(), |output: Child| {
+                        Stdio::from(output.stdout.unwrap())
+                    });
+
+                    let stdout = if commands.peek().is_some() {
+                        Stdio::piped()
+                    } else {
+                        Stdio::inherit()
+                    };
+
+                    let output = Command::new(command)
+                        .args(args)
+                        .stdin(stdin)
+                        .stdout(stdout)
+                        .spawn();
+
+                    match output {
+                        Ok(output) => {
+                            previous_command = Some(output);
+                        }
+                        Err(e) => {
+                            previous_command = None;
+                            eprintln!("{}", e);
+                        }
+                    };
                 }
             }
-            "exit" => return,
-            command => {
-                let child = Command::new(command).args(args).spawn();
+        }
 
-                match child {
-                    Ok(mut child) => {
-                        child.wait();
-                    }
-                    Err(e) => eprintln!("{}", e),
-                };
-            }
+        if let Some(mut final_command) = previous_command {
+            final_command.wait();
         }
     }
 }
