a10c4393909ec435352d17b83a40b1c4c9dcce29
Samuel Wilson <samWson@users.noreply.github.com>
2021-02-13 21:52:53 +1300

    Show prompt
    A note on the use of `ExecuteProcess`: it is the simplest but also
    most limited cross platform way of running an external command. It
    is synchronous so it will stop Two Shell from taking new commands
    before the current one has completed, which is what is required.
    If a more control over the external process is required in the
    future `TProcess` or `RunCommand` in the `process` unit are
    suitable alternatives.

--- src/twosh.pas
+++ src/twosh.pas
@@ -7,13 +7,29 @@ var
 	exitStatus: integer;
 	input: string;
 	path: string;
+	prompt: string;
+	stdout: Text;
 
 begin
-	Read(input);
+	// Assign standard output and open it for writing
+	Assign(stdout, '');
+	Rewrite(stdout);
 
+	// Print prompt
+	prompt := 'twosh > ';
+	Write(stdout, prompt);
+	Flush(stdout);
+
+	// Get user input from command line
+	Read(input);
 	command := Trim(input);
 
+	// Find the path of the executable command
 	path := ExeSearch(command, '');
 
+	// Execute the command
 	exitStatus := ExecuteProcess(path, '', []);
+
+	// Cleanup unmanaged resources
+	Close(stdout);
 end.
