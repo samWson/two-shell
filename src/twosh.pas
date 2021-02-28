2b971d74a55fd1f30c772095d01aaeefccb555e8
Samuel Wilson <samWson@users.noreply.github.com>
2021-02-28 22:01:20 +1300

    Pipes with TProcess signaling SIGPIPE broken pipe
    First attemps at making pipes.
    
    On line 123 the current process reads from stdout into the buffer.
    This works, however when the next process writes to stdin from the
    buffer a SIGPIPE signal is raised.
    
    The current process is not running after it is read and the attempt
    to write to the next process causes the broken pipe. See
    `man 7 pipe` *I/O on pipes and FIFOs* for details.
    
    This code follows the example from the Free Pascal wiki 'How to
    redirect output with TProcess':
    https://wiki.lazarus.freepascal.org/Executing_External_Programs#Using_input_and_output_of_a_TProcess
    
    I cannot figure out why the current process ends or how to keep
    its end of the pipe open for writing.
    
    The next attempt will make use of the FCL Pipes unit, which has
    `CreatePipeHandles` and `CreatePipeStreams`. This is possibly an
    alternative to TProcess.

--- src/twosh.pas
+++ src/twosh.pas
@@ -3,7 +3,7 @@ Program twosh;
 
 {$MODE OBJFPC}
 
-Uses process, sysutils, StrUtils;
+Uses process, sysutils, StrUtils, Types;
 
 {$WRITEABLECONST OFF}
 {$VARSTRINGCHECKS ON}
@@ -11,23 +11,30 @@ Uses process, sysutils, StrUtils;
 Const
   Prompt = 'twosh > ';
   DefaultString = '';
-  ProcessOptions = [poWaitOnExit];
-  // The shell will hang until the external process ends.
 
 Var
   args: Array Of RawByteString;
   command: string = DefaultString;
+  commands: TStringDynArray;
   executable: string = DefaultString;
-  executablePath: string = DefaultString;
   exitStatus: integer;
+  i: integer;
   input: string = DefaultString;
   parts: Array Of RawByteString;
-  processHandler: TProcess;
+  currentProcess: TProcess;
+  nextProcess: TProcess;
+	nextArgs: array of RawByteString;
+	nextParts: Array of RawByteString;
+	nextExecutable: string = DefaultString;
+	readSize: integer;
+	readCount: integer;
+	buffer: array[0..127] of char;
+	running: boolean;
 
 Begin
-  // Make an instance of an external processHandler handler
-  processHandler := TProcess.create(Nil);
-  processHandler.options := ProcessOptions;
+  // Make an instance of an external currentProcess handler
+  currentProcess := TProcess.create(Nil);
+  nextProcess := TProcess.create(Nil);
 
   Repeat
     Begin
@@ -36,45 +43,118 @@ Begin
 
       // Get user input from command line
       Readln(input);
-      command := Trim(input);
+      commands := SplitString(Trim(input), '|');
 
-      If Length(command) = 0 Then
-        continue;
+      //      If Length(commands) = 0 Then
+      //        continue;
 
-      parts := SplitCommandLine(command);
+      // iterate over each command
+      // We know if there is a next command if High(commands) - i = 0
+      For i := 0 To High(commands) Do
+        Begin
+          parts := SplitCommandLine(Trim(commands[i]));
 
-      // Separate the command from the arguments
-      executable := parts[0];
-      args := Copy(parts, 1, High(parts));
+          If Length(parts) = 0 Then
+            continue;
 
-      Case executable Of
-        'cd':
-              Begin
-                If Length(args) = 0 Then
-                  ChDir(GetUserDir()) // Default to users HOME directory
+          // Separate the command from the arguments
+          executable := parts[0];
+          args := Copy(parts, 1, High(parts));
+
+          Case executable Of
+            'cd':
+                  Begin
+                    If Length(args) = 0 Then
+                      ChDir(GetUserDir()) // Default to users HOME directory
+                    Else
+                      ChDir(args[0]) // Change to directory given as an argument to the command
+                  End;
+            'exit': exit();
+            Else
+              Try
+                // peek to see if there is another process
+                If High(commands) - i = 0 Then
+                  Begin
+                    // There is no next command piped after this one, output goes to shell stdout
+                    currentProcess.executable := executable;
+			// The shell will hang until the current process is finished
+                    currentProcess.options := [poWaitOnExit];
+                    currentProcess.parameters.addStrings(args);
+
+                    // Execute the command
+                    currentProcess.execute
+                  End
                 Else
-                  ChDir(args[0]) // Change to directory given as an argument to the command
-              End;
-        'exit': exit();
-        Else
-          Try
-            processHandler.executable := executable;
-            processHandler.parameters.addStrings(args);
 
-            // Execute the command
-            processHandler.execute
-          Except
-            on E: EProcess Do Writeln('Command `' + executable + '` failed');
-      End;
+            // There is another command piped after this one, output goes to the next commands stdin
+			begin
+                        currentProcess.executable := executable;
+			currentProcess.options := [poUsePipes];
+                        currentProcess.parameters.addStrings(args);
+
+			nextParts := SplitCommandLine(Trim(commands[i + 1]));
+			nextExecutable := nextParts[0];
+
+			nextProcess.executable := nextExecutable;
+			nextProcess.options := [poUsePipes];
+
+				if High(nextParts) > 1 then
+					begin
+                                            // there are arguments for the next command
+        					nextArgs := Copy(nextParts, 1, High(nextParts));
+        					nextProcess.parameters.addStrings(nextArgs)
+					end;
+
+			// Execute the processes
+			currentProcess.execute;
+			nextProcess.execute;
+
+			while currentProcess.running or (currentProcess.output.NumBytesAvailable > 0) do
+				begin
+					if currentProcess.Output.NumBytesAvailable > 0 then
+					begin
+						// make sure we don't read more data than is allocated in the buffer
+						readSize := currentProcess.Output.NumBytesAvailable;
+						if readSize > SizeOf(buffer) then
+							readSize := SizeOf(buffer);
+						
+						// Read the output into the buffer
+						// REVIEW: watch out for the index here. Not sure if it is right. Just the buffer may be all that is needed.
+						readCount := currentProcess.Output.Read(buffer[0], readSize);
+						running := currentProcess.Running;
+
+						// Write the buffer to the next process
+						nextProcess.Input.Write(buffer[0], readCount);
+
+						// REVIEW: if the next process writes too much dat to it's ouput
+						// then that data should be read here to prevent a deadlock.
+					end
+                                end;
+
+			// Close input on the next process so it finishes processing its data
+			nextProcess.CloseInput;
+			
+			// Wait for the next process to complete.
+			// REVIEW: This may not be a robust solution. Depending on the command
+			// being executed the process may not exit when its input is closed
+			// causing the following line to loop forever.
+			while nextProcess.Running do Sleep(1);
+
+			end
+              Except
+                on E: EProcess Do Writeln('Command `' + executable + '` failed');
+          End;
+        End;
+
+      // Clear input buffer
+      input := DefaultString;
+
+      currentProcess.parameters.clear();
     End;
-
-    // Clear input buffer
-    input := DefaultString;
-
-    processHandler.parameters.clear();
   End
 Until false;
 
 // Cleanup unmanaged resources
-processHandler.free();
+currentProcess.free();
+nextProcess.free();
 End.
